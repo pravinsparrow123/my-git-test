681b3d9f0a64e7fa96a5f41973c85337da6039df
pravinsparrow123 <pravinpk58585@gmail.com>
2024-08-01 14:51:03 +0530

    rule

---
+++ src/Blogs.js
@@ -0,0 +1,7 @@
+const Blogs = () =>
+{
+    return (
+        <h1>Blog page</h1>
+    );
+}
+export default Blogs;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Blogs from './Blogs';
+import Home from './Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter basename='/my-app-git'>
+      <Routes>
+        {/* <Route path="/" element={<App />}>
+         
+        </Route> */}
+         <Route index element={<Home />} />
+         <Route path="/blogs" element={<Blogs />} />
+      </Routes>
+   {/* <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode> */}
+  </BrowserRouter>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
